38f9eaf6678677d7b6a3ef2b3ca52796a65cf744
vinit gaur <31432392+vinit052@users.noreply.github.com>
2026-01-09 19:15:55 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React: Being build by Vinit Gaur</h1>
+      <h1>Vite + React: Being build by Vinitg</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
