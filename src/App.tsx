b8c1f487f09d4764491ba8fc790dd94681d5bcdb
Vinit <gaurvinit.2000@gmail.com>
2026-01-13 08:49:20 +0530

    Update for test in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn moree
+        Click on the Vite and React logos to learn moree. This used CI/CD to update github pages.
       </p>
     </>
   )
